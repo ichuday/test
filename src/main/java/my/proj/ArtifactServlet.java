4e9d47765962e1abddf8e7b9d4f1c442793a0554
teganalytics <38068166+teganalytics@users.noreply.github.com>
2018-05-02 16:23:00 +0530

    Update ArtifactServlet.java

--- src/main/java/my/proj/ArtifactServlet.java
+++ src/main/java/my/proj/ArtifactServlet.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "ArtifactServlet", value = "/home/support/Ad-eff")
+@WebServlet(name = "ArtifactServlet", value = "/home/support/Ad-eff/src/main/java/my/proj")
 public class ArtifactServlet extends HttpServlet {
 
   @Override
