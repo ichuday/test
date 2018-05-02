253d0c6fc49d72f5e8b7d38fcd8d46537184d1f3
teganalytics <38068166+teganalytics@users.noreply.github.com>
2018-05-02 16:34:14 +0530

    Update ArtifactServlet.java

--- src/main/java/my/proj/ArtifactServlet.java
+++ src/main/java/my/proj/ArtifactServlet.java
@@ -2,6 +2,7 @@ package my.proj;
 import java.io.IOException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "ArtifactServlet", value = "/home/support/Ad-eff/src/main/java/my/proj")
